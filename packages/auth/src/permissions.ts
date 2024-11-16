e74cdef612ec1e2f7b16c75a36d6d2acf3c3ba79
helenapaixao <hp.helenapaixao@gmail.com>
2024-11-16 13:17:57 -0300

    feat: Add permissions module for role-based access control

---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,16 @@
+import type { AbilityBuilder } from "@casl/ability"
+import type { AppAbility } from "."
+
+type Role = "ADMIN" | "MEMBER"
+
+type PermissionsByRole = (user: unknown, builder: AbilityBuilder<AppAbility>) => void
+
+export const permissions: Record<Role, PermissionsByRole> = {
+  ADMIN(_, {can}) {
+    can("manage", "all")
+
+   },
+  MEMBER(_,{can}){
+    can("invite","User")
+  }
+}
