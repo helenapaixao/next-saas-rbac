3dea18c8066a330ea91d56c68f389f6acf64f065
helenapaixao <hp.helenapaixao@gmail.com>
2024-11-18 20:58:41 -0300

    refactor: Update project subject types in auth module

--- packages/auth/src/subjects/project.ts
+++ packages/auth/src/subjects/project.ts
@@ -1,6 +1,12 @@
-import {z} from 'zod'
+import { z } from 'zod'
 export const projectSubject = z.tuple([
-  z.union([z.literal("create"), z.literal("delete"), z.literal("manage")]),
-  z.literal("Project")
+  z.union([
+    z.literal('manage'),
+    z.literal('get'),
+    z.literal('create'),
+    z.literal('update'),
+    z.literal('delete'),
+  ]),
+  z.literal('Project'),
 ])
 export type ProjectSubject = z.infer<typeof projectSubject>
