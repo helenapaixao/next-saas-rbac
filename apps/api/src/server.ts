dc8744ff65c627c1f3e8a8e523f4fb485c3a6e5f
helenapaixao <hp.helenapaixao@gmail.com>
2024-11-18 23:00:40 -0300

    feat: Add createAccount route to auth module

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -6,12 +6,18 @@ serializerCompiler,
 validatorCompiler,
 type ZodTypeProvider
 } from "fastify-type-provider-zod"
+import { createAccount } from "./http/routes/auth/create-account";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+
 app.register(fastifyCors)
+app.register(createAccount)
+
+
 
 app.listen({port: 3333}).then(() => {
   console.log("HTTP server running")
