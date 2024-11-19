127084fbc6c0caa0f85309b19b4eee3074dd2b73
helenapaixao <hp.helenapaixao@gmail.com>
2024-11-18 22:23:58 -0300

    refactor: Remove unused project model in auth module

--- packages/auth/src/models/projects.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod'
-export const projectSchema = z.object({
-  __typename: z.literal("Project").default("Project"),
-  id: z.string(),
-  ownerId: z.string(),
-})
-export type User = z.infer<typeof projectSchema>
