85c5fa4bd941cac24ee24013096bb85d38f9ef67
helenapaixao <hp.helenapaixao@gmail.com>
2024-11-18 22:23:37 -0300

    refactor: Update project subject types and add projectSchema to projectSubject in auth module

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,9 @@
-import { defineAbilityFor} from "@saas/auth"
+import { defineAbilityFor, projectSchema } from '@saas/auth'
 
- const ability = defineAbilityFor({role: "ADMIN"})
+const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
 
-const userCanInviteSomeoneElse = ability.can("invite", "User")
+const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-id' })
 
-
-const userCanDeleteOtherUsers = ability.can("delete", "User")
-
-const userCannotDeleteOthersUsers = ability.cannot('delete', "User")
-
-console.log(userCanInviteSomeoneElse)
-console.log(userCanDeleteOtherUsers)
+console.log(ability.can('get', 'Billing'))
+console.log(ability.can('create', 'Invite'))
+console.log(ability.can('delete', project))
